1eddbbf8f3782bd459b072b9a65eb153fb1fc4d4
Eduardo Ramos <eduardoramos15975@gmail.com>
2024-08-17 13:16:11 -0300

    User

---
+++ src/main/java/com/eduardo/commerce/entities/User.java
@@ -0,0 +1,26 @@
+package com.eduardo.commerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+
+}
