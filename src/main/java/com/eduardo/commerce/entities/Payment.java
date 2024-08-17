1e641a4eac042db8c0fb9586af573dfba8b036b2
Eduardo Ramos <eduardoramos15975@gmail.com>
2024-08-17 20:52:17 -0300

    Payment e associacao OneToOne

--- src/main/java/com/eduardo/commerce/entities/Payment.java
+++ src/main/java/com/eduardo/commerce/entities/Payment.java
@@ -12,21 +12,16 @@ import java.time.Instant;
 @AllArgsConstructor
 
 @Entity
-@Table(name = "tb_order")
-public class Order {
+@Table(name = "tb_payment")
+public class Payment {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-    private Instant moment;
-    private OrderStatus status;
+    private Instant instant;
 
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private User client;
-
-    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-    private Payment payment;
-
+    @OneToOne
+    @MapsId
+    private Order order;
 }
