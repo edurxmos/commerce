91e12172f439e6734af7e19fbd72b6897bd4f474
Eduardo Ramos <eduardoramos15975@gmail.com>
2024-08-31 18:44:14 -0300

    Buscar produtos por páginas

--- src/main/java/com/eduardo/commerce/services/ProductService.java
+++ src/main/java/com/eduardo/commerce/services/ProductService.java
@@ -4,6 +4,8 @@ import com.eduardo.commerce.dto.ProductDTO;
 import com.eduardo.commerce.entities.Product;
 import com.eduardo.commerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +23,12 @@ public class ProductService {
         Product result = product.get();
         ProductDTO dto = new ProductDTO(result);
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 
 }
