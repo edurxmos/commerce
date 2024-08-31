8d570188b43ff60cf637930f4abceb8cb838399d
Eduardo Ramos <eduardoramos15975@gmail.com>
2024-08-31 19:16:17 -0300

    Método POST

--- src/main/java/com/eduardo/commerce/controllers/ProductController.java
+++ src/main/java/com/eduardo/commerce/controllers/ProductController.java
@@ -5,10 +5,7 @@ import com.eduardo.commerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -27,5 +24,11 @@ public class ProductController {
     public Page<ProductDTO> findAll(Pageable pageable) {
         return service.findAll(pageable);
     }
+
+    @PostMapping
+    public ProductDTO insert(@RequestBody ProductDTO dto) {
+        return service.insert(dto);
+    }
+
 
 }
