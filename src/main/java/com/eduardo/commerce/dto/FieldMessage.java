d21749fde0f04e8c9eaff78d9ab8df7c6b1d995d
Eduardo Ramos <eduardoramos15975@gmail.com>
2024-09-15 21:43:44 -0300

    Customizando a resposta da validação

---
+++ src/main/java/com/eduardo/commerce/dto/FieldMessage.java
@@ -0,0 +1,13 @@
+package com.eduardo.commerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+
+public class FieldMessage {
+
+    private String fieldName;
+    private String message;
+}
